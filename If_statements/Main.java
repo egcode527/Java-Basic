5a8e545d368e48b2bc37b4aa997d2e05f07137ac
gaurav-regmi <regmi.gaurav527@gmail.com>
2026-08-13 13:12:47 -0500

    if statements practice

---
+++ If_statements/Main.java
@@ -0,0 +1,21 @@
+package If_statements;
+
+public class Main {
+
+    /*
+    If Statements Practice Questions
+
+    1. Write a Java program to check whether a number is positive, negative, or zero.
+    2. Write a Java program to check whether a number is even or odd.
+    3. Write a Java program to check whether a number is positive and even.
+    4. Write a Java program to find the maximum of two numbers.
+    5. Write a Java program to check whether three numbers are in increasing order.
+
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
