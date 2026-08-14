9703a04ee0b1c2822bdef81fc8b71e5b5e4e25b2
gaurav-regmi <regmi.gaurav527@gmail.com>
2026-08-13 19:39:16 -0500

    Practice part2

--- If_statements/Main.java
+++ If_statements/Main.java
@@ -8,10 +8,6 @@ public class Main {
     If Statements Practice Questions
 
     1. Write a Java program to check whether a number is positive, negative, or zero.
-    2. Write a Java program to check whether a number is even or odd.
-    3. Write a Java program to check whether a number is positive and even.
-    4. Write a Java program to find the maximum of two numbers.
-    5. Write a Java program to check whether three numbers are in increasing order.
 
      */
 
